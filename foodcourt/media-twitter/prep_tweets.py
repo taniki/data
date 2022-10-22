196e2d250dafb48ffac8b0b49cbf07f42e94ca42
taniki <tk@deveha.com>
2022-10-22 22:25:15 +0200

    bouge les meubles pour un gros pivot

---
+++ foodcourt/media-twitter/prep_tweets.py
@@ -0,0 +1,64 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import pandas as pd
+from tqdm.notebook import trange, tqdm
+
+# %%
+from minet.twitter import TwitterAPIScraper
+scraper = TwitterAPIScraper()
+
+# %%
+accounts = [
+    'lemondefr',
+    'mediapart',
+    'mediapartblogs',
+    'lesjoursfr',
+    'blast_france',
+    'brutofficiel',
+    'loopsidernews'
+]
+
+since = '2022-01-01'
+
+# %%
+dfs = {}
+
+for account in accounts:
+    tweets = list(scraper.search_tweets(f'from:@{account} since:{since}'))
+    dfs[account] = pd.DataFrame.from_records(tweets)
+
+# %%
+for account, tweets in dfs.items():
+    tweets.to_parquet(f'datasets/{account}.par', index=False)
+    tweets.to_csv(f'datasets/{account}.csv', index=False)
+
+# %%
+tweets = (
+    pd
+    .concat(list(dfs.values()))
+    .set_index('id')
+    .assign(
+        local_time=lambda d: pd.to_datetime(d.local_time),
+        user_screen_name = lambda df: df.user_screen_name.astype('category')
+    )
+)
+
+tweets
+
+# %%
+tweets.to_parquet('datasets/tweets.par')
+tweets.to_csv('datasets/tweets.csv')
--- readme.md
+++
@@ -1,3 +0,0 @@
-# médias et économie de l'attention
-
-Une exploration sur l'économie de l'attention et de l'économie des médias (indépendants)
